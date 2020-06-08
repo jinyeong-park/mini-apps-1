a046b46740eb83a8301a10186cfbeaa02436849f
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-08 12:46:05 -0700

    draw X in the clicked cell

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
+console.log('Hello App');
+
+//add a click event listener for each square on the board.
+//When a square is clicked, add an X to that square.
+function tdclick(id) {
+  //player start first
+  console.log('id: ' + id);
+  //draw x in html browser
+}
 
 
