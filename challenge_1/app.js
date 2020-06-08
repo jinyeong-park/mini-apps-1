aa40ce0b018fe67f399de1aa1b107bba1995e73e
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-08 15:04:47 -0700

    found winner

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,74 @@
+// - the first move always starts with player X
+// - the app detects a win or tie and displays an appropriate message
+// - a button resets the game for a new round of gameplay
+
+// A single page app means that once the page loads,
+//no user-generated actions on the page may cause the entire page to reload.
 console.log('Hello App');
 
 //add a click event listener for each square on the board.
 //When a square is clicked, add an X to that square.
-function tdclick(id) {
-  //player start first
-  console.log('id: ' + id);
-  //draw x in html browser
+
+/*----- event listeners -----*/
+let currentPlayer = "X";
+let gameActive = true;
+// 3 * 3 board
+//let board = [['', '', ''], ['', '', ''], ['', '', '']];
+let board = ['', '', '', '', '', '', '', '', ''];
+//0, 1, 2, 3, 4, 5, 6, 7, 8
+
+function cellclick(id) {
+
+  //check id when clicking certain cell.
+    console.log('id: ' + id);
+
+    console.log('current player :', currentPlayer)
+    //change their turn to play the game
+    if (currentPlayer === "X") {
+      //Change the HTML content of element with id to "X" in the browser
+      document.getElementById(id).innerHTML = "X";
+      board[id] = "X";
+      currentPlayer = "O";
+      console.log(board)
+    } else {
+      document.getElementById(id).innerHTML = "O";
+      board[id] = "O";
+      currentPlayer = "X";
+      console.log(board)
+    }
+    //Winning
+    //if current player has taken the same line of row, column, or digonal =>
+    //winner is decided.
+
+  //   //row
+    for (let i = 0 ; i < board.length; i +=3) {
+      if (board[i] === "X" && board[i] === board[i+1] && board[i+1] === board[i+2]) alert(`${board[i]} is winner`);
+      if (board[i] === "O" && board[i] === board[i+1] && board[i+1] === board[i+2]) alert(`${board[i]} is winner`);
+    }
+
+   //column
+    for (let i = 0; i < 3; i++) {
+      if (board[i] === "X" && board[i] === board[i+3] && board[i+3] === board[i+6]) alert(`${board[i]} is winner`);
+      if (board[i] === "O" && board[i] === board[i+3] && board[i+3] === board[i+6]) alert(`${board[i]} is winner`);
+    }
+
+    //diagonal1
+    if (board[0] === "X" && board[0] === board[4] && board[4] === board[8]) alert(`${board[0]} is winner`);
+    if (board[0] === "O" && board[0] === board[4] && board[4] === board[8]) alert(`${board[0]} is winner`);
+
+  //    //diagonal2
+    if (board[2] === "X" && board[2] === board[4] && board[4] === board[6]) alert(`${board[2]} is winner`);
+    if (board[2] === "O" && board[2] === board[4] && board[4] === board[6]) alert(`${board[2]} is winner`);
+
+
+  gameActive = false;
+  board = ['', '', '', '', '', '', '', '', ''];
+  return board;
+
+
 }
+
+
+/*----- functions -----*/
 
 
