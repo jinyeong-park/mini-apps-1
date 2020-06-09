c1533124f905d407a4ed62be962bdacd2f8b0e2e
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-08 18:42:22 -0700

    cleanup

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -21,7 +21,7 @@ let board = ['', '', '', '', '', '', '', '', ''];
 // var player2 = prompt("PlayerO enter your name");
 
 
-function cellclick(id) {
+function cellClick(id) {
 
   //check id when clicking certain cell.
     console.log('id: ' + id);
@@ -46,44 +46,40 @@ function cellclick(id) {
 
     //row
     for (let i = 0 ; i < board.length; i +=3) {
-      if (board[i] === "X" && board[i] === board[i+1] && board[i+1] === board[i+2]) document.getElementById('status').innerHTML = `${board[i]} is winner`;
-      if (board[i] === "O" && board[i] === board[i+1] && board[i+1] === board[i+2]) document.getElementById('status').innerHTML = `${board[i]} is winner`;
+      if (board[i] === "X" && board[i] === board[i+1] && board[i+1] === board[i+2]) document.getElementById('status').innerHTML = `${board[i]} is winner!`;
+      if (board[i] === "O" && board[i] === board[i+1] && board[i+1] === board[i+2]) document.getElementById('status').innerHTML = `${board[i]} is winner!`;
     }
 
     //column
     for (let i = 0; i < 3; i++) {
-      if (board[i] === "X" && board[i] === board[i+3] && board[i+3] === board[i+6]) document.getElementById('status').innerHTML = `${board[i]} is winner`;
-      if (board[i] === "O" && board[i] === board[i+3] && board[i+3] === board[i+6]) document.getElementById('status').innerHTML = `${board[i]} is winner`;
+      if (board[i] === "X" && board[i] === board[i+3] && board[i+3] === board[i+6]) document.getElementById('status').innerHTML = `${board[i]} is winner!`;
+      if (board[i] === "O" && board[i] === board[i+3] && board[i+3] === board[i+6]) document.getElementById('status').innerHTML = `${board[i]} is winner!`;
     }
 
-    //diagonal1
-    if (board[0] === "X" && board[0] === board[4] && board[4] === board[8]) document.getElementById('status').innerHTML = `${board[0]} is winner`;
-    if (board[0] === "O" && board[0] === board[4] && board[4] === board[8]) document.getElementById('status').innerHTML = `${board[0]} is winner`;
+    //diagonal1(major)
+    if (board[0] === "X" && board[0] === board[4] && board[4] === board[8]) document.getElementById('status').innerHTML = `${board[0]} is winner!`;
+    if (board[0] === "O" && board[0] === board[4] && board[4] === board[8]) document.getElementById('status').innerHTML = `${board[0]} is winner!`;
 
-   //diagonal2
-    if (board[2] === "X" && board[2] === board[4] && board[4] === board[6]) document.getElementById('status').innerHTML = `${board[2]} is winner`;
-    if (board[2] === "O" && board[2] === board[4] && board[4] === board[6]) document.getElementById('status').innerHTML = `${board[0]} is winner`;
+   //diagonal2(minor)
+    if (board[2] === "X" && board[2] === board[4] && board[4] === board[6]) document.getElementById('status').innerHTML = `${board[2]} is winner!`;
+    if (board[2] === "O" && board[2] === board[4] && board[4] === board[6]) document.getElementById('status').innerHTML = `${board[0]} is winner!`;
 
   gameActive = false;
   //Once winner is decided or board is fully occupied, cannot click anymore
 
-
 }
 
 var reset_game = function () {
+  //reset effect
   //remove board in the browser
   for (let i = 0; i < board.length; i++) {
     document.getElementById(i).innerHTML = '';
 
   }
-  //remove board
+  //make board itself empty
   board = ['', '', '', '', '', '', '', '', ''];
   // return board;
 
 }
-
-
-
-/*----- functions -----*/
 
 
